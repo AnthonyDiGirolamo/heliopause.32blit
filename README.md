5350bf3a72327d1c5900fb306df5c760b2784822
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2021-11-24 16:08:58 -0800

    Add pigweed bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Heliopause for 32blit
+# 32blit Project
 
+## Clone the repo
+
+```sh
+git clone https://github.com/AnthonyDiGirolamo/heliopause.32blit
+cd heliopause.32blit
+
+git submodule update --init
+cd third_party/pico-sdk
+git submodule update --init
+```
+
+## Install the toolchains
+
+This step downloads compilers and tools like `arm-none-eabi-gcc` and `cmake`. It also creates a Python virtualenv and installs the `32blit-tools` package.
+
+The first run will take a while. Everything will be installed inside the project directory under `.environment`.
+
+**Linux or Mac**
+
+```sh
+. ./bootstrap.sh
+```
+
+**Windows**
+
+```sh
+bootstrap.bat
+```
+
+The downloads are handled using [CIPD](https://chromium.googlesource.com/infra/luci/luci-go/+/refs/heads/main/cipd/). Artifacts are cached (usually in `~/.cipd-cache-dir`) so subsequent bootstrap runs will be faster. To see the full list of installed packages see [the json files in this directory](https://cs.opensource.google/pigweed/pigweed/+/master:pw_env_setup/py/pw_env_setup/cipd_setup/pigweed.json).
+
+More info on bootstrap can be found in the [Pigweed Getting Started docs](https://pigweed.dev/docs/getting_started.html).
+
+## Build all the things
+
+1. Activate the environment
+
+   After the first bootstrap you can run.
+
+   ```sh
+   . ./activate.sh
+   ```
+
+   To update your shell's environment variables and `PATH`.
+
+2. Compile
+
+   If you have the [fish shell](https://fishshell.com/) installed just run `go.fish`. Otherwise you can build your 32blit project as normal.
+
+   ```sh
+   mkdir -p build
+   cd build
+   cmake ..
+   make
+   ```
--- go.fish
+++ go.fish
@@ -1,36 +1,10 @@
 #!/usr/bin/env fish
-set -x PIGWEED_EXPERIMENTAL_ROOT /mnt/auxdata/anthony/pigweed/experimental
-set -x PW_BRANDING_BANNER /mnt/auxdata/anthony/pigweed/experimental/banner.txt
-set -x PW_BRANDING_BANNER_COLOR magenta
-set -x PW_PROJECT_ROOT /mnt/auxdata/anthony/pigweed/experimental
-set -x PW_ROOT /mnt/auxdata/anthony/pigweed/experimental/third_party/pigweed
-set -x _PW_ACTUAL_ENVIRONMENT_ROOT /mnt/auxdata/anthony/pigweed/experimental/.environment
-set -x PW_CIPD_INSTALL_DIR /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd
-set -x CIPD_CACHE_DIR /home/anthony/.cipd-cache-dir
-set -x PW_PIGWEED_CIPD_INSTALL_DIR /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/pigweed
-set -x PW_BAZEL_CIPD_INSTALL_DIR /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/bazel
-set -x PW_PYTHON_CIPD_INSTALL_DIR /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/python
-set -x PW_LUCI_CIPD_INSTALL_DIR /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/luci
-set -x VIRTUAL_ENV /mnt/auxdata/anthony/pigweed/experimental/.environment/pigweed-venv
-
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/pigweed
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/pigweed/bin
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/bazel
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/bazel/bin
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/python
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/python/bin
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/luci
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/cipd/luci/bin
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/.environment/pigweed-venv/bin
-set -p PATH /mnt/auxdata/anthony/pigweed/experimental/third_party/pigweed/out/host/host_tools
-
-set -x PICO_SDK_PATH /mnt/auxdata/anthony/pico-sdk
-set -x PICO_EXTRAS_PATH /mnt/auxdata/anthony/pico-extras
-set -x 32BLIT_DIR /mnt/auxdata/anthony/32blit-sdk
-
 set -x PW_PROJECT_DIR (realpath (dirname (status --filename)))
 cd $PW_PROJECT_DIR
+
+set -x PICO_SDK_PATH $PW_PROJECT_DIR/third_party/pico-sdk
+set -x 32BLIT_DIR $PW_PROJECT_DIR/third_party/32blit-sdk
+
 set -x COMMON_ARGS -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -D32BLIT_DIR=$32BLIT_DIR
 
 mkdir -p build.pico
