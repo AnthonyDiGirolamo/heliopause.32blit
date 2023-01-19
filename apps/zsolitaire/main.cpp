cd805298cf1d5752dfa1a37a283653035aa4f5d9
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2023-01-19 08:59:35 -0800

    lib_colors: endesga64 naming

--- apps/zsolitaire/main.cpp
+++ apps/zsolitaire/main.cpp
@@ -1,43 +1,26 @@
 #include "main.hpp"
 
 #include "32blit.hpp"
+#include "pw_random/random.h"
+#include "pw_random/xor_shift.h"
 
 #include "assets.hpp"
 #include "card_font.hpp"
-#include "pw_random/random.h"
-#include "pw_random/xor_shift.h"
+#include "colors.hpp"
 #include "random.hpp"
 
 using namespace blit;
 
-Pen cream_on_transparent[] = {
-  Pen(0, 0, 0, 0),       // clear
-  Pen(0xf9, 0xe6, 0xcf),
+enum SpritePaletteColor {
+  BG = 0,
+  FG = 1,
 };
 
-Pen white_on_transparent[] = {
-    Pen(0, 0, 0, 0),       // clear
-    Pen(0xff, 0xf1, 0xe8), // #fff1e8 WHITE
-};
+Pen clear = Pen(0, 0, 0, 0);
 
-Pen gray_on_transparent[] = {
-    Pen(0, 0, 0, 0),       // clear
-    Pen(0x3d, 0x3d, 0x3d), // #3d3d3d
-};
-
-Pen green_on_lightgreen[] = {
-    Pen(0x33, 0x98, 0x4b), // #33984b
-    Pen(0x1e, 0x6f, 0x50), // #1e6f50
-};
-
-Pen darkgreen_on_transparent[] = {
-  Pen(0, 0, 0, 0), // clear,
-  Pen(0x13, 0x4c, 0x4c), // #134c4c
-};
-
-Pen darkgreen_on_green[] = {
-  Pen(0x1e, 0x6f, 0x50), // #1e6f50
-  Pen(0x13, 0x4c, 0x4c), // #134c4c
+Pen sprite_colors[]{
+    Pen(0, 0, 0, 0), // clear
+    Pen(0xff, 0xff, 0xff, 255),
 };
 
 const blit::Font kCardFont(mrmotext);
@@ -58,10 +41,14 @@ void init() {
   blit::set_screen_mode(ScreenMode::hires);
 
   screen.sprites = Surface::load(asset_sprite_sheet);
+  screen.sprites->palette = sprite_colors;
 
-  map_card = TileMap::load_tmx((uint8_t *)asset_map_cards, screen.sprites, Layer_CardOutline);
-  map_holder = TileMap::load_tmx((uint8_t *)asset_map_cards, screen.sprites, Layer_HolderOutline);
-  map_holder_background = TileMap::load_tmx((uint8_t *)asset_map_cards, screen.sprites, Layer_HolderBackground);
+  map_card = TileMap::load_tmx((uint8_t *)asset_map_cards, screen.sprites,
+                               Layer_CardOutline);
+  map_holder = TileMap::load_tmx((uint8_t *)asset_map_cards, screen.sprites,
+                                 Layer_HolderOutline);
+  map_holder_background = TileMap::load_tmx(
+      (uint8_t *)asset_map_cards, screen.sprites, Layer_HolderBackground);
 
   // Test sprite index fetch
   printf("tile offset: %d %d %d\n", map_card->tile_at(Point(0, 0)),
@@ -75,42 +62,48 @@ void draw_map(TileMap *map, Point pos) {
       uint8_t tile_index = map->tile_at(tile_pos);
       if (tile_index == 0)
         continue;
-      screen.sprite(tile_index, pos + Point(8*tile_pos.x, 8*tile_pos.y));
+      screen.sprite(tile_index, pos + Point(8 * tile_pos.x, 8 * tile_pos.y));
     }
   }
 }
 
 void draw_card(int x, int y, bool holder = false) {
   Point pos(x, y);
-  Point pos_shadow(x+2, y+2);
+  Point pos_shadow(x + 2, y + 2);
   Size map_size(5, 7);
-  Size size(8*map_size.w, 8*map_size.h);
+  Size size(8 * map_size.w, 8 * map_size.h);
   Rect card_rect(pos, size);
 
   if (holder) {
     // Holder
-    screen.sprites->palette = darkgreen_on_transparent;
+    sprite_colors[FG] = ENDESGA64[Ocean2];
+    sprite_colors[BG] = clear;
     // map_holder->draw(&screen, card_rect, nullptr);
     draw_map(map_holder, pos);
     // Holder background
-    screen.sprites->palette = darkgreen_on_green;
+    sprite_colors[FG] = ENDESGA64[Ocean1];
+    sprite_colors[BG] = ENDESGA64[Ocean2];
     // map_holder_background->draw(&screen, card_rect, nullptr);
     draw_map(map_holder_background, pos);
   }
 
   // Card Shadow
-  screen.sprites->palette = gray_on_transparent;
-  draw_map(map_card, pos + Point(1, 1));
+  sprite_colors[FG] = ENDESGA64[Coffee0];
+  sprite_colors[FG].a = 128;
+  sprite_colors[BG] = clear;
+  draw_map(map_card, pos + Point(2, 2));
 
   // Card background color
-  screen.pen = white_on_transparent[1];
+  screen.pen = ENDESGA64[Coffee7];
   int bg_offset = 5;
   Point bg_corner1 = pos + Point(bg_offset, bg_offset);
-  Point bg_corner2 = bg_corner1 + Point(size.w - bg_offset*2, size.h - bg_offset*2);
+  Point bg_corner2 =
+      bg_corner1 + Point(size.w - bg_offset * 2, size.h - bg_offset * 2);
   screen.rectangle(Rect(bg_corner1, bg_corner2));
 
-  // Card
-  screen.sprites->palette = cream_on_transparent;
+  // Card Border
+  sprite_colors[FG] = ENDESGA64[Coffee6];
+  sprite_colors[BG] = clear;
   // map_card->draw(&screen, card_rect, nullptr);
   draw_map(map_card, pos);
 
@@ -128,7 +121,9 @@ void render(uint32_t time_ms) {
   screen.clear();
 
   // Reset seed
-  screen.sprites->palette = green_on_lightgreen;
+  sprite_colors[FG] = ENDESGA64[Ocean0];
+  sprite_colors[BG] = ENDESGA64[Ocean1];
+
   rng = pw::random::XorShiftStarRng64(123456);
   // Draw background
   for (int x = 0; x < screen.bounds.w; x += 8) {
@@ -140,10 +135,11 @@ void render(uint32_t time_ms) {
     }
   }
 
-  draw_card(0, 0, true);
-  draw_card(0, 14, false);
-  draw_card(0, 14+14, false);
-
+  // Test card drawing
+  draw_card(24, 0, true);
+  draw_card(16, 14, false);
+  draw_card(8, 14 + 14, false);
+  draw_card(0, 14 + 14 + 14, false);
 }
 
 void update(uint32_t time) {
--- libs/lib_colors/include/colors.hpp
+++ libs/lib_colors/include/colors.hpp
@@ -24,3 +24,70 @@ extern Pen ENDESGA64[];
 #define PICO8_INDIGO PICO8[13]
 #define PICO8_PINK PICO8[14]
 #define PICO8_PEACH PICO8[16]
+
+enum Endesga64Colors {
+  Blood,         // 00 #ff0040
+  Black0,        // 01 #131313
+  Black1,        // 02 #1b1b1b
+  Gray0,         // 03 #272727
+  Gray1,         // 04 #3d3d3d
+  Gray2,         // 05 #5d5d5d
+  Gray3,         // 06 #858585
+  Gray4,         // 07 #b4b4b4
+  White,         // 08 #ffffff
+  Steel6,        // 09 #c7cfdd
+  Steel5,        // 10 #92a1b9
+  Steel4,        // 11 #657392
+  Steel3,        // 12 #424c6e
+  Steel2,        // 13 #2a2f4e
+  Steel1,        // 14 #1a1932
+  Steel0,        // 15 #0e071b
+  Coffee0,       // 16 #1c121c
+  Coffee1,       // 17 #391f21
+  Coffee2,       // 18 #5d2c28
+  Coffee3,       // 19 #8a4836
+  Coffee4,       // 20 #bf6f4a
+  Coffee5,       // 21 #e69c69
+  Coffee6,       // 22 #f6ca9f
+  Coffee7,       // 23 #f9e6cf
+  Orange3,       // 24 #edab50
+  Orange2,       // 25 #e07438
+  Orange1,       // 26 #c64524
+  Orange0,       // 27 #8e251d
+  BrightOrange0, // 28 #ff5000
+  BrightOrange1, // 29 #ed7614
+  BrightOrange2, // 30 #ffa214
+  Yellow0,       // 31 #ffc825
+  Yellow1,       // 32 #ffeb57
+  Green5,        // 33 #d3fc7e
+  Green4,        // 34 #99e65f
+  Green3,        // 35 #5ac54f
+  Green2,        // 36 #33984b
+  Green1,        // 37 #1e6f50
+  Green0,        // 38 #134c4c
+  Ocean0,        // 39 #0c2e44
+  Ocean1,        // 40 #00396d
+  Ocean2,        // 41 #0069aa
+  Ocean3,        // 42 #0098dc
+  Ocean4,        // 43 #00cdf9
+  Ocean5,        // 44 #0cf1ff
+  Ocean6,        // 45 #94fdff
+  CandyGrape3,   // 46 #fdd2ed
+  CandyGrape2,   // 47 #f389f5
+  CandyGrape1,   // 48 #db3ffd
+  CandyGrape0,   // 49 #7a09fa
+  RoyalBlue2,    // 50 #3003d9
+  RoyalBlue1,    // 51 #0c0293
+  RoyalBlue0,    // 52 #03193f
+  Purple0,       // 53 #3b1443
+  Purple1,       // 54 #622461
+  Purple2,       // 55 #93388f
+  Purple3,       // 56 #ca52c9
+  Salmon0,       // 57 #c85086
+  Salmon1,       // 58 #f68187
+  Red4,          // 59 #f5555d
+  Red3,          // 60 #ea323c
+  Red2,          // 61 #c42430
+  Red1,          // 62 #891e2b
+  Red0,          // 63 #571c27
+};
